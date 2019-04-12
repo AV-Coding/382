b8df95591f597925a05e7a6b2cc0dd562c39f15a
Rick Mercer <rhm12399@users.noreply.github.com>
2019-04-11 21:06:44 -0700

    Starter Code

---
+++ TicTacToe/TicTacToe.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class TicTacToe {
+
+public:
+  void startGame() {
+    cout << "Play a game of tic tac toe";
+  }
+
+};
---
+++ Wumpus/Wumpus.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Wumpus {
+
+public:
+  void startGame() {
+    cout << "Play a game of Wumpus\n";
+  }
+
+};
